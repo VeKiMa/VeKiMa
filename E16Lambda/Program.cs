41f5d2adcc29bea1bca4b94fd4b543ac8bd36af4
VeKiMa <149431810+VeKiMa@users.noreply.github.com>
2024-02-08 00:07:50 +0100

    Add files via upload

---
+++ E16Lambda/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UčenjeCS.E16Lambda
+{
+    internal class Program
+    {
+
+        public Program()  {
+            Console.WriteLine(KlasicnaMetoda(3));
+
+            var kvadrat = (int b) => b * b;
+
+            Console.WriteLine(kvadrat(3));
+
+            var algoritam = (int x, int y) =>
+                {
+                    var t = x++ - y;
+                    return x + y + t;
+                };
+
+            Console.WriteLine(algoritam(2,3));
+
+
+            var lista = new List<Smjer>() {
+                new Smjer() {Naziv="Prvi"},
+                new Smjer() {Naziv="Drugi"},
+            };
+
+
+            lista.ForEach(s => {
+                Console.WriteLine(s.Naziv);
+            });
+
+            lista.ForEach(Console.WriteLine);
+            
+                
+            
+        }
+        private int KlasicnaMetoda(int b) 
+        {
+            return b*b;
+        }
+
+
+    }
+}
