26c3f546d157f870afdbe9f2af34b2696f8237ef
VeKiMa <149431810+VeKiMa@users.noreply.github.com>
2024-02-07 22:27:52 +0100

    Add files via upload

---
+++ Grupa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UčenjeCS.E15KonzolnaAplikacija.Model
+{
+    internal class Grupa
+    {
+    }
+}
